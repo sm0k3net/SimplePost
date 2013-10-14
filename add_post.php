7c6e8f4b312e914f5eea093ff55f7537f6e4fcc7
sm0k3 <sm0k3web@users.noreply.github.com>
2013-10-14 22:08:49 +0300

    add_post.php - sample page with form to input data

---
+++ add_post.php
@@ -0,0 +1,13 @@
+<html>
+ <head><title>Sample page</title></head>
+ <body>
+  <form action="post.php" method="POST">
+    <h5>Title:</h5>&nbsp;<input type="text" name="title" /><br />
+    <h5>Message:</h5>&nbsp;<textarea name="message"> </textarea><br />
+    <h5>Status:</h5><input type="text" name="status" /><br />
+    <p><input type="submit" value="Add update" /></p>
+    <p><a href="#">Back to main page</a></p>
+  </form>
+ </body>
+</html>
+<!-- This is sample page with form to input data into blog -->
